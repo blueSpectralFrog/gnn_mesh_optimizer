Fixed nan values appearing because of collapsing elements. Fixed Issue with config file lr as str instead of float. Added normalization of edge features.

physics/utils_potential_energy.py
@@ -99,6 +99,21 @@ def compute_def_gradient_element(element: jnp.ndarray, ref_coords: jnp.ndarray, 
 
         return dN_dxi  # Shape: (8, 3)
 
+    # def _safe_inverse(J, tol=1e-8):
+
+    #     def inv_single(Ji):
+    #         U, S, Vh = jnp.linalg.svd(Ji)
+    #         S_inv = jnp.where(S > tol, 1.0 / S, 0.0)
+    #         return (Vh.T * S_inv) @ U.T
+    
+    #     return jax.vmap(inv_single)(J)
+    
+    def _safe_inverse(J, tol=1e-8):
+
+        U, S, Vh = jnp.linalg.svd(J)
+        S_inv = jnp.where(S > tol, 1.0 / S, 0.0)
+        return (Vh.T * S_inv) @ U.T
+
     def _compute_deformation_gradient(X, x, gauss_point=(0, 0, 0)):
         """
         Compute deformation gradient F at a Gauss point.
@@ -115,7 +130,7 @@ def compute_def_gradient_element(element: jnp.ndarray, ref_coords: jnp.ndarray, 
 
         # Jacobian in reference configuration
         J0 = X.T @ dN_dxi  # (3, 3)
-        invJ0 = jnp.linalg.inv(J0)
+        invJ0 = _safe_inverse(J0)
 
         # Gradients of shape functions in physical space
         dN_dX = dN_dxi @ invJ0  # (8, 3)

training/train.py
@@ -4,6 +4,8 @@ from jax import jit
 import optax
 import haiku as hk
 
+import pdb
+
 from functools import partial
 from absl import logging
 
@@ -27,22 +29,19 @@ def compute_loss_pinn(params, theta_tuple, pred_fn, ref_geom_data, external_forc
     theta_norm, theta = theta_tuple
     Upred = pred_fn(params, theta_norm)
 
-
-    ################# HEREREREREREREE #####################
     return total_potential_energy(Upred, theta, ref_geom_data, external_forces)
-    #######################################################
 
 def train_step(params, opt_state, theta_tuple, optimiser, loss_fn):
     """Train emulator for one theta input point """
 
     partial_loss_fn = partial(loss_fn, theta_tuple=theta_tuple)
-    grad_fn = jax.value_and_grad(partial_loss_fn)
 
-    ##################################
-    ##################################
-    ##################################
-    
+    grad_fn = jax.value_and_grad(partial_loss_fn)
     loss, grads = grad_fn(params)
+    
+    jax.tree_util.tree_map(lambda g: jnp.isnan(g).any(), grads)
+    jax.debug.print("Grad shape: {}", jax.tree_util.tree_map(lambda x: x.shape, grads))
+    
     updates, opt_state = optimiser.update(grads, opt_state)
     params = optax.apply_updates(params, updates)
     return params, opt_state, loss
@@ -82,7 +81,7 @@ class PhysicsLearner:
                                      external_forces=external_forces)
 
         # jit the training step function for faster execution
-        self.train_step = jit(partial(train_step,
+        self.train_step = (partial(train_step,
                                       optimiser = self.optimiser,
                                       loss_fn = self.train_loss_fn))
 