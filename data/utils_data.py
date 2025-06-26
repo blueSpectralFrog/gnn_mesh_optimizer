fixed volume calculation, added in the boundary function, working to implement it on the correct nodes

data/utils_data.py
@@ -152,12 +152,15 @@ class ReferenceGeometry:
             element_vol = 0
             for n_tet in ELEMENT_VOLUME_BKDOWN[graph_inputs.cell_type]:
                 # keep in mind that self.init_node_position is now the size of the training nodeset
-                element_vol += self.element_volume(graph_inputs.node_position[:,0,:][element[jnp.array(n_tet)]])
+                element_vol += self.element_volume(self.init_chosen_node_position[element[jnp.array(n_tet)]])
             self.elements_vol = self.elements_vol.at[index].set(element_vol)
 
         from data.constitutive_law import isotropic_elastic, J_transformation_fn
         self.constitutive_law = isotropic_elastic
         self.Jtransform = J_transformation_fn
+        self.boundary_adjust_fn = dirichlet_bd_coords = lambda U: U*graph_inputs.vertex_data[graph_inputs.nodes_unique_to_training]
+        
+        # coordinates of dirichlet: self.init_chosen_node_position[jnp.where((jnp.multiply(graph_inputs.nodes_unique_to_training,jnp.hstack(graph_inputs.vertex_data[graph_inputs.nodes_unique_to_training])))!=0)[0]]
         
         # TODO:
         # Virtual nodes implementation? Need for larger meshes

models/encoder.py
@@ -89,7 +89,7 @@ class PrimalGraphEmulator(nn.Module):
     n_total_nodes: int
     output_dim: int
     # real_node_indices: Sequence[bool]
-    # boundary_adjust_fn: Sequence[bool] = None
+    boundary_adjust_fn: Sequence[bool] = None
 
     @nn.compact
     def __call__(self, V: jnp.ndarray, E: jnp.ndarray, theta: jnp.ndarray, sow_latents=False):
@@ -159,8 +159,10 @@ class PrimalGraphEmulator(nn.Module):
         Upred = jnp.hstack(individual_mlp_predictions)
 
         # adjust predictions to account for displacement boundary conditions
-        # if self.boundary_adjust_fn is not None:
-        #     Upred = self.boundary_adjust_fn(Upred)
+        if self.boundary_adjust_fn is not None:
+            Upred = self.boundary_adjust_fn(Upred)
+
+        thing = jnp.where(jnp.sum(self.boundary_adjust_fn(Upred), axis=1)!=0)[0]
 
         # return displacment prediction array
         return Upred